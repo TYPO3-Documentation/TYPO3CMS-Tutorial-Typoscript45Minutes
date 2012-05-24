71cf2a4884b758b670ed705b1d8e30689fb9b3b5
Christopher <chrissitopher@gmx.de>
2012-05-24 20:47:49 +0200

    [TASK] Migrate doc_tut_ts45 1.0.0
    Import doc_tut_ts45 1.0.0.
    
    Change-Id: Ib511dd6f1401d42d86c6bcf109d940561c722614

---
+++ doc_tut_ts45/ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "doc_tut_ts45".
+#
+# Auto generated 21-03-2010 20:23
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'TypoScript in 45 minutes',
+	'description' => 'Learn the fundamentals of TypoScript in just 45 minutes',
+	'category' => 'doc',
+	'shy' => 0,
+	'version' => '1.0.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Official Documentation',
+	'author_email' => 'documentation@typo3.org',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"984e";s:12:"ext_icon.gif";s:4:"7277";s:20:"doc/doc_core_api.pdf";s:4:"2f49";s:20:"doc/doc_core_api.txt";s:4:"e5a2";s:14:"doc/manual.sxw";s:4:"2182";}',
+	'suggests' => array(
+	),
+);
+
+?>
