f0268d5cd4e10e0d84befbd69fa577495bede7fd
Christopher <chrissitopher@gmx.de>
2012-05-24 20:48:56 +0200

    [TASK] Migrate doc_tut_ts45 1.1.0
    Import doc_tut_ts45 1.1.0.
    
    Change-Id: I5cb5f03c32c86f3ce6cc11a2c1bdd5af29ea6905

--- doc_tut_ts45/ext_emconf.php
+++ doc_tut_ts45/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "doc_tut_ts45".
 #
-# Auto generated 21-03-2010 20:23
+# Auto generated 06-02-2011 15:34
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Learn the fundamentals of TypoScript in just 45 minutes',
 	'category' => 'doc',
 	'shy' => 0,
-	'version' => '1.0.0',
+	'version' => '1.1.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"984e";s:12:"ext_icon.gif";s:4:"7277";s:20:"doc/doc_core_api.pdf";s:4:"2f49";s:20:"doc/doc_core_api.txt";s:4:"e5a2";s:14:"doc/manual.sxw";s:4:"2182";}',
+	'_md5_values_when_last_written' => 'a:6:{s:9:"ChangeLog";s:4:"a4c3";s:12:"ext_icon.gif";s:4:"7277";s:14:"doc/manual.odt";s:4:"ff15";s:14:"doc/manual.pdf";s:4:"1134";s:14:"doc/manual.sxw";s:4:"721b";s:14:"doc/manual.txt";s:4:"d09e";}',
 	'suggests' => array(
 	),
 );
